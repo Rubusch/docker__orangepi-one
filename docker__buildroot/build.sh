b001ff6619634d8c29a7ef22ff5a77f3b3d59b15
Lothar Rubusch <l.rubusch@gmx.ch>
2020-05-10 20:42:22 +0200

    build.sh: update

--- docker__buildroot/build.sh
+++ docker__buildroot/build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -e
 export DEFCONFIG=lothars__orangepi_one_defconfig
-cd buildroot
+export MY_HOME=/home/$(whoami)
+
+sudo chown $(whoami):$(whoami) -R ${MY_HOME}/buildroot/dl
+sudo chown $(whoami):$(whoami) -R ${MY_HOME}/buildroot/output
+
+cd ${MY_HOME}/buildroot
 make ${DEFCONFIG}
 make -j8
