017abe13d71366194d2ecb23362907722dff0b88
Lothar Rubusch <l.rubusch@gmail.com>
2021-06-28 22:18:02 +0200

    docker: checkout all sources to host
    Signed-off-by: Lothar Rubusch <l.rubusch@gmail.com>

--- docker__buildroot/build_context/build.sh
+++ docker__buildroot/build_context/build.sh
@@ -1,16 +1,52 @@
 #!/bin/bash -e
-DEFCONFIG=lothars__orangepi_one_defconfig
-MY_HOME=/home/$(whoami)
+#
+#
+
+MY_USER=$(whoami)
+DEFCONFIG="lothars__orangepi_one_defconfig"
+MY_HOME="/home/${MY_USER}"
 BUILDROOT_DIR="${MY_HOME}/buildroot"
 DL_DIR="${BUILDROOT_DIR}/dl"
 OUTPUT_DIR="${BUILDROOT_DIR}/output"
-SHARES=( ${DL_DIR} ${OUTPUT_DIR} )
+SSH_DIR="${MY_HOME}/.ssh"
+SSH_KNOWN_HOSTS="${SSH_DIR}/known_hosts"
+#BR_BRANCH="lothar/orangepi-devel"
+BR_BRANCH="lothar/2020.11.x"
 
-## fix permissions (takes time)
-for item in ${SHARES[*]}; do
-    test -e ${item} && sudo chown $(whoami).$(whoami) -R ${item}
+
+## permissions
+for item in "${BUILDROOT_DIR}" "${MY_HOME}/.gitconfig" "${SSH_DIR}"; do
+    test -e "${item}" || continue
+    if [ ! "${MY_USER}" == "$( stat -c %U ${item} )" ]; then
+        ## may take some time
+        sudo chown ${MY_USER}:${MY_USER} -R ${item}
+    fi
 done
 
+
+## ssh known_hosts
+touch ${SSH_KNOWN_HOSTS}
+for item in "github.com" "bitbucket.org"; do
+    if [ "" == "$( grep ${item} -r ${SSH_KNOWN_HOSTS} )" ]; then
+        ssh-keyscan ${item} >> "${SSH_KNOWN_HOSTS}"
+    fi
+done
+
+
+## initial clone
+if [[ "1" == "$(find ${YOCTO_DIR} | wc -l)" ]]; then
+    cd ${MY_HOME}
+
+    ## try it...
+    git clone -j4 --depth=1 --branch ${BR_BRANCH} https://github.com/Rubusch/buildroot.git ${BUILDROOT_DIR}
+
+    ## alternatively get official buildrooot
+    ## NB: this will fail to build, since kconfig won't be able to build for cross device environments
+    ## (the mounted 'dl' and 'output' folders - I [quick]fixed this in my repo)
+    #git clone -j4 --depth=1 https://github.com/buildroot/buildroot.git ${BUILDROOT_DIR}
+fi
+
+## build
 cd ${BUILDROOT_DIR}
 make ${DEFCONFIG}
 make -j8
