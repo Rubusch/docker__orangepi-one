9e5bce25006c1b5823d093b82ab885d81a2d1f94
Lothar Rubusch <l.rubusch@gmail.com>
2021-10-28 18:47:14 +0200

    setup.sh: fix paths

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 build()
 {
-	CONTAINER_NAME="$(grep "container_name:" -r ./${1}/docker-compose.yml | awk -F: '{ print $2 }' | tr -d ' ')"
+	CONTAINER_NAME="$(grep "container_name:" -r "${1}/docker-compose.yml" | awk -F: '{ print $2 }' | tr -d ' ')"
 	pushd "${1}" &> /dev/null
 	echo "UID=$(id -u)" > .env
 	echo "GID=$(id -g)" >> .env
@@ -19,9 +19,9 @@ DRYRUN="${1}"
 IMAGE="sandbox"
 test -z "${DOCKERDIR}" && DOCKERDIR="docker"
 
-TAG="$(grep "ENV DOCKER_BASE_TAG" -r "./${DOCKERDIR}/build_context/Dockerfile" | awk -F= '{ print $2 }' | tr -d '"')"
+TAG="$(grep "ENV DOCKER_BASE_TAG" -r "${DOCKERDIR}/build_context/Dockerfile" | awk -F= '{ print $2 }' | tr -d '"')"
 
-CONTAINER="$(docker images | grep "/${IMAGE}" | grep "${TAG}" | awk '{print $3}')"
+CONTAINER="$(docker images | grep "${IMAGE}" | grep "${TAG}" | awk '{print $3}')"
 
 if [ -z "${CONTAINER}" ]; then
 	git clone "https://github.com/Rubusch/docker__${IMAGE}.git" "${IMAGE}"
