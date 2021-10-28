fbe39083662ecea4b55ee8d2ffc72dbd94b5cdc6
Lothar Rubusch <l.rubusch@gmail.com>
2021-10-28 18:37:15 +0200

    setup.sh: fix exit code and bashism
    Signed-off-by: Lothar Rubusch <l.rubusch@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,13 +1,14 @@
 #!/bin/sh -e
 build()
 {
+	CONTAINER_NAME="$(grep "container_name:" -r ./${1}/docker-compose.yml | awk -F: '{ print $2 }' | tr -d ' ')"
 	pushd "${1}" &> /dev/null
 	echo "UID=$(id -u)" > .env
 	echo "GID=$(id -g)" >> .env
 	if [ -n "${2}" ]; then
 	    docker-compose build
 	else
-	    docker-compose up
+	    docker-compose up --exit-code-from "${CONTAINER_NAME}"
 	fi
 	popd &> /dev/null
 }
@@ -18,8 +19,7 @@ DRYRUN="${1}"
 IMAGE="sandbox"
 test -z "${DOCKERDIR}" && DOCKERDIR="docker"
 
-TAG="$(grep "ENV DOCKER_BASE_TAG" -r "./${DOCKERDIR}/build_context/Dockerfile" | awk -F= '{ print $2 }')"
-TAG="${TAG//\"}"
+TAG="$(grep "ENV DOCKER_BASE_TAG" -r "./${DOCKERDIR}/build_context/Dockerfile" | awk -F= '{ print $2 }' | tr -d '"')"
 
 CONTAINER="$(docker images | grep "/${IMAGE}" | grep "${TAG}" | awk '{print $3}')"
 
@@ -32,4 +32,3 @@ fi
 build "./${DOCKERDIR}" "${DRYRUN}"
 
 echo "READY."
-
